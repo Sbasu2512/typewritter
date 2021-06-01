e2553603ae5da0d3c56977aa388fb1e54b77719f
Sayantan Basu <sayantanbasu@live.in>
2021-06-01 14:56:00 -0400

    final commit

--- typewritter.js
+++ typewritter.js
@@ -3,9 +3,9 @@ const sentence = "hello there from lighthouse labs";
 const animate = str => {
   let time = 1000 ;
   setTimeout(()=>{
-    for (let ele of str){
-      process.stdout.write(ele);
-      time = time + 500 ;
+    for (let i=0; i<str.length; i++){
+      process.stdout.write(str[i]);
+      time += 500 ;         // time = time + 500
       }
   },time);
 }
