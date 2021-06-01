694c5d7b8b9166955d1e2ce3d9f1bafc89156a90
Sayantan Basu <sayantanbasu@live.in>
2021-06-01 14:13:56 -0400

    added fn

--- typewritter.js
+++ typewritter.js
@@ -1,5 +1,13 @@
 const sentence = "hello there from lighthouse labs";
 
 const animate = str => {
-
+  let time = 1000 ;
+  setTimeout(()=>{
+    for (let ele of str){
+      process.stdout.write(ele);
+      time = time + 300 ;
+      }
+  },time);
 }
+
+animate(sentence);
